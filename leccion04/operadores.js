cb287305ddb60891f992e79a291b83e968cbd649
developerCanver <canver199@gmail.com>
2021-02-20 21:44:58 -0500

    Tipos de variables}

---
+++ leccion04/operadores.js
@@ -0,0 +1,54 @@
+let a=3, b=2;
+z= a+b;
+console.log("suma es:"+ z);
+
+z= a*b;
+console.log("multiplcar es:"+ z);
+
+z= a-b;
+console.log("resta es:"+ z);
+
+z= a/b;
+console.log("division es:"+ z);
+
+z= a%b;//resiciduo de la division operador de modulo
+console.log("resiciduo de la division :"+ z);
+
+z= a**b;//operador de exponente
+console.log("Exponente: "+ z);
+
+
+ a=3, b=2;
+z = a+b;
+
+//incremento
+//pre-incremento (el operadro ++ anr¿tes de la variable)
+ z = ++a;
+ console.log(a);
+ console.log(z);
+
+ //post incremento
+ z = b++;
+ console.log(b);
+ console.log(z);
+
+ a=3, b=2;
+ z = a+b;
+ 
+ //decremento
+ //pre-decremento (el operadro ++ anr¿tes de la variable)
+  z = --a;
+  console.log(a);
+  console.log(z);
+ 
+  //post decremento
+  z = b--;
+  console.log(b);
+  console.log(z);
+
+
+  //operadores
+  a=3, b= 2, c=1;
+
+  z = a * b +c;
+  console.log(z);
