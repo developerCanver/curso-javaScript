016af365d78812560ff63e5828d5714635da3780
developerCanver <canver199@gmail.com>
2021-02-21 21:23:24 -0500

    v2 condiiones

--- leccion03/operadores.js
+++ leccion03/operadores.js
@@ -47,8 +47,28 @@ z = a+b;
   console.log(z);
 
 
-  //operadores
-  a=3, b= 2, c=1;
+  //operadores operando ordenes de operacions que ejecuta javascript
+  a=3, b= 2, c=1, d=2;
 
-  z = a * b +c;
+  z = a * b + c / d;
   console.log(z);
+///se ejecuta 1 la multiplicacion 2 la division 3 la suma o resta
+  z = (a * b + c) / d;
+  console.log(z);// los parentesis ronpen las ejecucion de operando
+
+
+
+  //operadoeres aritmeticos
+
+  let o = 1;
+
+  //operador de asignacion incrementar+´´
+  o += 3;
+  console.log(o);
+/*
+
+*=
+/=
+-=
+
+*/
---
+++ leccion03/operadoresComparacion.js
@@ -0,0 +1,34 @@
+ /*
+comparador de operaciones 
+
+== si son valores iguales
+=== si son valores iguales y tipos de variables
+! = si son diferentes los valores
+!== si son diferentes los valores y tipos
+>=
+
+ */
+let a=3, b=2,c="3";
+let z = a == b;
+console.log(z);
+z = a === c;
+console.log(z);
+//verifica sis vlos valores son iguales y tambien los tipos
+
+
+a = 3, b = 2, c = "3";
+z = a < b;
+console.log(z);
+
+if (a % 2 == 0) {
+  console.log("Es numero par");
+}else{
+  console.log("El numero es impar");
+}
+
+let valMin = 0, valMax = 10;
+if (a >= valMin && a <= valMax) {
+  console.log("Dentro del rango");
+} else {
+  console.log("Fuera del rango");
+}
