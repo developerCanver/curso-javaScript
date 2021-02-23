0c262f3ed9a098b4b66bb3e97594218730efe264
developerCanver <canver199@gmail.com>
2021-02-22 21:22:26 -0500

    v2 condiciones

--- leccion04/condiciones.js
+++ leccion04/condiciones.js
@@ -6,3 +6,43 @@ if (conditcion) {
     console.log("condicion falsa");
     
 }
+
+//ejmplo 2
+
+let mnumero = 3;
+if (numero == 1) {
+    console.log(" Numero 1");
+    
+} else if(numero == 2) {
+    console.log("Numero 2");
+
+    
+}else if(numero == 3) {
+    console.log("Numero 3");
+
+    
+}else if(numero == 4) {
+    console.log("Numero 4");
+
+    
+}else{
+
+    console.log("Numero desconocido");
+
+}
+
+let mes = 4;
+let estacion;
+
+
+if (mes == 1 || mes == 2 || mes == 12) {
+    console.log("Invierno");
+} else if(mes == 3 || mes == 4 || mes == 5 ){
+    console.log("Primavera");
+}else if(mes == 6 || mes == 7 || mes == 8 ){
+    console.log("verano");
+}else if(mes == 9 || mes == 10 || mes == 11 ){
+    console.log("otoño");
+}else{
+    console.log("valor incorrecto");
+}
